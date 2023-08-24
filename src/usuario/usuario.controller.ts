20b233d59e8360e8d3480cc73ac9f6246d77bd52
mdhenriques <md.henriques@outlook.com>
2023-08-24 16:31:33 -0300

    Filtrando dados no GET

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,9 @@
 import { Get, Body, Controller, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { CriaUsuarioDTO } from "./dto/criaUsuario.dto";
+import { UsuarioEntity } from "./usuario.entity";
+import { v4 as uuid } from 'uuid';
+import { ListaUsuarioDTO } from "./dto/ListaUsuario.dto";
 
 @Controller('/usuarios')
 export class UsuarioController{
@@ -9,12 +12,29 @@ export class UsuarioController{
 
    @Post() 
    async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
-        this.usuarioRepository.salvar(dadosDoUsuario);
-        return dadosDoUsuario; 
+        const usuarioEntity = new UsuarioEntity();
+        usuarioEntity.email = dadosDoUsuario.email;
+        usuarioEntity.senha = dadosDoUsuario.senha;
+        usuarioEntity.nome = dadosDoUsuario.nome;  
+        usuarioEntity.id = uuid();
+
+        this.usuarioRepository.salvar(usuarioEntity);
+        return { 
+          id: usuarioEntity.id,
+          message: 'usuario criado com sucesso'
+        }  
    } 
 
    @Get()
    async listaUsuarios() {
-        return this.usuarioRepository.listar();
+        const usuariosSalvos = await this.usuarioRepository.listar();
+        const usuariosLista = usuariosSalvos.map(
+          usuario => new ListaUsuarioDTO(
+               usuario.id,
+               usuario.nome
+          )
+        );
+
+        return usuariosLista;
    }
 }
