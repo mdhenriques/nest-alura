516dac095c90e40654f9c10cc45c3533debd2ff8
mdhenriques <md.henriques@outlook.com>
2023-08-24 12:15:45 -0300

    Criando usuarioRepository

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UsuarioRepository } from "./usuario.repository";
 
 @Controller('/usuarios')
 export class UsuarioController{
 
+    private usuarioRepository = new UsuarioRepository();
+
    @Post() 
-   async criaUsuario() {
-        return 'usuario-criado!'; 
+   async criaUsuario(@Body() dadosDoUsuario) {
+        this.usuarioRepository.salvar(dadosDoUsuario);
+        return dadosDoUsuario; 
    } 
 }
