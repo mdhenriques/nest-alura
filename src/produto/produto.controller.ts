9b52848ec9c8f87aa7e479f535476c64e3d3391e
mdhenriques <md.henriques@outlook.com>
2023-08-24 14:36:59 -0300

    Criando rotas para produtos

---
+++ src/produto/produto.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Body, Get, Post } from "@nestjs/common";
+import { ProdutoRepository } from "./produto.repository";
+
+
+@Controller('/produtos')
+export class ProdutoController{
+    
+    constructor(private produtoRepository: ProdutoRepository) {}
+
+    @Post()
+    async criaProduto(@Body() dadosDoProduto) {
+        this.produtoRepository.salvar(dadosDoProduto);
+        return dadosDoProduto;
+    }
+
+    @Get()
+    async listaProdutos() {
+        return this.produtoRepository.listar();
+    }
+}
